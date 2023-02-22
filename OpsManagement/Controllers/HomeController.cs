bfbc92fab456ff0f0b74e31ad5b897f50fde801f
hutdast <nikensonmidi@gmail.com>
2023-02-22 17:55:55 -0500

    initial commit

---
+++ OpsManagement/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using OpsManagement.Models;
+using System.Web.Mvc;
+
+namespace OpsManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(User user)
+        {
+            return View(user);
+        }
+
+        public ActionResult Login(User user)
+        {
+            if(user == null) { user= new User { IsAuthenticated = false}; }
+           if(!string.IsNullOrWhiteSpace(user.UserName) || !string.IsNullOrWhiteSpace(user.UserName))
+            {
+                user.IsAuthenticated= true;
+            }
+            return RedirectToAction("Index", "Home",user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# DodoBedSystems
