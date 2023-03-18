7838d9ff359646b19e860d013a966b6fb4aad57f
hutdast <nikensonmidi@gmail.com>
2023-03-18 18:47:33 -0400

    removed

--- OpsManagement/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpsManagement.Models
-{
-    public class User
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public bool IsAuthenticated { get; set; }
-    }
-}
